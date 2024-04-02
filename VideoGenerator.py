498e16151c1c30c4c87ccf1c1b5571811a7aa433
xiazi <77074100@qq.com>
2024-04-02 20:40:43 +0900

    position

--- VideoGenerator.py
+++ VideoGenerator.py
@@ -3,7 +3,7 @@ from moviepy.editor import ImageClip, AudioFileClip, concatenate_videoclips, Tex
 class VideoGenerator:
     def __init__(self, output_file="output_video.mp4"):
         self.output_file = output_file
-    
+
     def generate_video(self, media_info, intro_texts=None, font="Meiryo", fps=24):
         if len(media_info) == 0:
             print("media_info is empty.")
@@ -16,13 +16,17 @@ class VideoGenerator:
             img_clip = ImageClip(media["image"]).set_duration(media["audio_duration"])
             audio_clip = AudioFileClip(media["audio_file"]).set_duration(media["audio_duration"])
             img_clip = img_clip.set_audio(audio_clip)
-            img_clip = img_clip.fadein(0.3).fadeout(0.3)
+            img_clip = img_clip.fadein(0.5).fadeout(0.5)
 
             text = intro_texts["text"]
-            position = intro_texts["position"]
+            # position = intro_texts["position"]
 
-            txt_clip = TextClip(text, fontsize=36, color='white', font=font, size=(img_clip.w - 20, None), method='caption')
-            txt_clip = txt_clip.set_duration(media["audio_duration"]).set_position(position)
+            txt_clip = TextClip(text, fontsize=36, color='orange', font=font, size=(img_clip.w - 20, None), method='caption')
+            text_width, text_height = txt_clip.size
+            def custom_position(t, clip=img_clip, height=text_height):
+                return ('center', clip.size[1] - height - 20)
+            txt_clip = txt_clip.set_duration(media["audio_duration"]).set_position(custom_position)
+
             img_clip = CompositeVideoClip([img_clip, txt_clip])
             clips.append(img_clip)
 
